610305b3c1e317f289b111a348898ef012d8b8c5
Gustavo Barbosa <gustavolimaeduardo@gmail.com>
2021-05-12 20:29:27 -0300

    phptips ep01

---
+++ phptips/ep01/source/Config.php
@@ -0,0 +1,5 @@
+<?php
+
+require '../vendor/autoload.php';
+
+echo "<h1> Ola Mundo... </h1>";
---
+++ phptips/ep01/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '986792fb7a151c0971836cbd0eb803352039d919',
+    'name' => 'gustavoeduardo/phptips001',
+  ),
+  'versions' => 
+  array (
+    'gustavoeduardo/phptips001' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '986792fb7a151c0971836cbd0eb803352039d919',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.4.1',
+      'version' => '6.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9256f12d8fb0cd0500f93b19e18c356906cbed3d',
+    ),
+  ),
+);
